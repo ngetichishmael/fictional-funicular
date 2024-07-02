37a22dee3d3bee94f1ce89325d4d822a7f4e6d7e
ngetichishmael <168198499+antonymbaya44@users.noreply.github.com>
2024-07-02 19:56:35 +0300

    Fictional Funicular

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<School/>
+    </>
+  )
+}
+function School(){
+    return(
+        <>
+            <div className='zindua'>
+                <h1>Zindua School</h1>
+                <h2>Coding Bootcamp</h2>
+            </div>
+
+        </>
+    )
+}
+
+export default App
